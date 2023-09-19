63b3c26e2ab4a611c62bd9a5a4d605282235b9ab
ChauveSouris <desseobscurite@gmail.com>
2023-09-19 23:48:08 +0300

    Created private func

--- SliderGame/ContentView.swift
+++ SliderGame/ContentView.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    let targetValue = 0 //целевое
+    let currentValue = 0.0 //от слайдера
+    
     var body: some View {
         VStack {
-            Text("Подвиньте слайдер как можно ближе к: ")
+            Text("Подвиньте слайдер как можно ближе к: \(targetValue)")
+            
+            ButtonView(title: "Проверить ответ", action: {})
+
+            ButtonView(title: "Начать заново", action: {})
         }
         .padding()
+    }
+    
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(currentValue))
+        return 100 - difference
     }
 }
 
