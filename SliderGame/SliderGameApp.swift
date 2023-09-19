ca3e5fb532a3d0c2f20df43c4861ae22105838ae
ChauveSouris <desseobscurite@gmail.com>
2023-09-19 23:20:40 +0300

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Елизавета Медведева on 19.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
