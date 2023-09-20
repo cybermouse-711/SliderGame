9a5a4ce483b6f3ad82e9ad50bb07c1053318cbb3
ChauveSouris <desseobscurite@gmail.com>
2023-09-20 21:52:12 +0300

    Created Slider Fail

---
+++ SliderGame/SliderUIK.swift
@@ -0,0 +1,45 @@
+//
+//  SliderUIK.swift
+//  SliderGame
+//
+//  Created by Елизавета Медведева on 20.09.2023.
+//
+
+import SwiftUI
+
+struct SliderUIK: UIViewRepresentable {
+    @Binding var currentValue: Double
+    
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider()
+        slider.maximumValue = 100
+        slider.minimumValue = 0
+        slider.thumbTintColor = .red
+        
+        return slider
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        uiView.value = Float(currentValue)
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(currentValue: $currentValue)
+    }
+}
+
+extension SliderUIK {
+    final class Coordinator: NSObject {
+        @Binding var currentValue: Double
+        
+        init(currentValue: Binding<Double>) {
+            self._currentValue = currentValue
+        }
+    }
+}
+
+struct SliderUIK_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderUIK(currentValue: .constant(50))
+    }
+}
